b0b3208d42ace00a41c4eeee86c2a4eb44463967
Davidryt <myphonedavid@gmail.com>
2021-02-22 14:20:13 +0100

    nada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component ({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  
 })
-export class AppComponent {
-  title = 'practica01';
-}
+  export class AppComponent {
+  title = 'COMANDA'; 
+  subtitle = 'Consumiciones';
+  seleccionado: string = "";
+  costeTotal = 0;
+  
+  public comidas:Array<[string, number]>=[['Ternera con bambú y setas',7.0], ['Arroz tres delicias',3.5], ['',4.4]]
+  
+  public comidaSeleccionada=[]; 
+  public costecomidaseleccionada=[];
+
+  }
