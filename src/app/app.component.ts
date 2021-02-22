4f5dd76bdafcf17854cc60aacc99c8bc030cd20c
Ivan Fernandez <100383564@alumnos.uc3m.es>
2021-02-22 19:53:52 +0100

    Selector, adding and list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-@Component ({
+@Component({
 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-  
+
 })
-  export class AppComponent {
-  title = 'COMANDA'; 
+export class AppComponent {
+  title = 'COMANDA';
   subtitle = 'Consumiciones';
   seleccionado: string = "";
   costeTotal = 0;
-  
-  public comidas:Array<[string, number]>=[['Ternera con bambú y setas',7.0], ['Arroz tres delicias',3.5], ['',4.4]]
-  
-  public comidaSeleccionada=[]; 
-  public costecomidaseleccionada=[];
 
+  public comidas: Array<[string, number]> = [['Ternera con bambú y setas', 7.0], ['Arroz tres delicias', 3.5], ['', 4.4]]
+
+  public comidaSeleccionada = 'Ternera con bambú y setas';
+  public consumiciones: Array<[string, number]> = [];
+
+  public changeSelected(comida: string) {
+    this.comidaSeleccionada = comida;
   }
+
+  public getConsumiciones() {
+    return this.consumiciones;
+  }
+
+  public getComidas() {
+    return this.comidas;
+  }
+
+  public addConsumicion() {
+    this.consumiciones.push(this.comidas.find((comida) => comida[0] === this.comidaSeleccionada) || ['', 0]);
+  }
+}
