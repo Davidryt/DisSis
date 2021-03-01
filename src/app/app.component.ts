6d6f0611438dcfc602fb7ff87c823c9ebbed7746
DAVID RICO MENENDEZ <100384036@alumnos.uc3m.es>
2021-03-01 09:43:46 +0100

    left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,19 @@ import { Component } from '@angular/core';
   export class AppComponent {
   title = 'COMANDA'; 
   subtitle = 'Consumiciones';
-  seleccionado: string = "";
+  seleccionado: string = '';
   costeTotal = 0;
   
   public comidas:Array<[string, number]>=[['Ternera con bambú y setas',7.0], ['Arroz tres delicias',3.5], ['',4.4]]
   
   public comidaSeleccionada=[]; 
   public costecomidaseleccionada=[];
+
+  public addLinea(){
+    //hacer
+  }
+  public delLinea(){
+    //hacer
+  }
 
   }
