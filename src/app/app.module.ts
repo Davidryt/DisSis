0b1d1f1e22eae027202bbe30faea8957e2723dd6
Ivan Fernandez <100383564@alumnos.uc3m.es>
2021-03-10 18:53:44 +0100

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './filter.pipe'; // -> imported filter pipe
 
+// NECESARIO PARA PIPE CURRENCY!:
+import { LOCALE_ID } from '@angular/core';
+import '@angular/common/locales/global/es';
 @NgModule({
   declarations: [
-    AppComponent,
-    FilterPipe // -> added filter pipe to use it inside the component
+    AppComponent, FilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
