5d335ca41ab88224ca9ba1e233f8cf992622dc29
DAVID RICO MENENDEZ <100384036@alumnos.uc3m.es>
2021-03-10 17:44:19 +0100

    nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
-
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-
 })
 export class AppComponent {
-  title = 'COMANDA';
-  subtitle = 'Consumiciones';
-  seleccionado: string = "";
-  costeTotal = 0;
-
-  public comidas: Array<[string, number]> = [['Ternera con bambú y setas', 7.0], ['Arroz tres delicias', 3.5], ['Pato a la naranja', 14.4]]
-
-  public comidaSeleccionada = 'Ternera con bambú y setas';
-  public consumiciones: Array<[string, number]> = [];
-
-  public changeSelected(comida: string) {
-    this.comidaSeleccionada = comida;
-  }
-
-  public getConsumiciones() {
-    return this.consumiciones;
-  }
-
-  public getComidas() {
-    return this.comidas;
-  }
-
-  public getTotal() {
-    let total:number = 0;
-    this.consumiciones.forEach((consumicion) => total+=consumicion[1]);
-    return total;
-  }
-
-  public addConsumicion() {
-    this.consumiciones.push(this.comidas.find((comida) => comida[0] === this.comidaSeleccionada) || ['', 0]);
-  }
-  public deleteConsumicion(index: number) {
-    this.consumiciones.splice(index, 1);
-  }
+  title = 'Car Comparator Filter';
+  searchText = '';
+  characters = [
+    'AUDI',
+    'TESLA',
+    'MERCEDES BENZ',
+    'SEAT',
+    'TOYOTA',
+    'COCHES',
+    'NOSE',
+    'NO_ENTIENDO_DE_COCHES'
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-// NECESARIO PARA PIPE CURRENCY!:
-import { LOCALE_ID } from '@angular/core';
-import '@angular/common/locales/global/es';
+import { AppComponent } from './app.component';
+
+import { FilterPipe } from './filter.pipe'; // -> imported filter pipe
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe // -> added filter pipe to use it inside the component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
-  providers: [ { provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
