b7ddd8eebd7b6a8d72b27b1280cd3e3b18d9125a
Ivan Fernandez <100383564@alumnos.uc3m.es>
2021-03-10 19:15:51 +0100

    colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   public EstadoCoche: any = EstadoCoche; // ESTA LINEA SOLO SIRVE PARA PODER USAR EstadoCoche en el TEMPLATE.
   public coches: Array<Coche> = [
     new Coche('renault', 'scenic', new Date(2007, 10, 1), new Date(2018, 3, 1), 6500, 'coche01.jpg', EstadoCoche.BUENO),
-    new Coche('SEAT', 'ibiza', new Date(2011, 10, 1), new Date(2019, 3, 1), 12000, 'coche02.jpg', EstadoCoche.BUENO),
+    new Coche('SEAT', 'ibiza', new Date(2011, 10, 1), new Date(2019, 3, 1), 12000, 'coche02.jpg', EstadoCoche.MALO),
     new Coche('renault', 'laguna', new Date(2012, 10, 1), new Date(2017, 1, 1), 3000, 'coche03.jpg', EstadoCoche.BUENO),
     new Coche('TESLA', 'model 3', new Date(2017, 10, 1), new Date(2020, 5, 1), 45000, 'coche04.jpg', EstadoCoche.BUENO)
   ];
