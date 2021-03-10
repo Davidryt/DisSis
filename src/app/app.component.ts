ace379edd3688077c1d7cb5dbe8c3ee87fade225
Ivan Fernandez <100383564@alumnos.uc3m.es>
2021-03-10 18:34:05 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Coche, EstadoCoche } from './coche/coche';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public EstadoCoche: any = EstadoCoche; // ESTA LINEA SOLO SIRVE PARA PODER USAR EstadoCoche en el TEMPLATE.
+  public coches: Array<Coche> = [
+    new Coche('renault', 'scenic', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, 'coche01.jpg', EstadoCoche.BUENO),
+    new Coche('renault', 'scenic', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, 'coche01.jpg', EstadoCoche.BUENO),
+    new Coche('renault', 'scenic', new Date(2007, 10, 1), new Date(2018, 3, 1), 5000, 'coche01.jpg', EstadoCoche.BUENO)
+  ];
   title = 'Car Comparator Filter';
   searchText = '';
   characters = [
